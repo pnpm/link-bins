0c0e9d9e9c8653e970a3f086534de611ff5bd71f
Zoltan Kochan <z@kochan.io>
2018-06-19 22:48:33 +0300

    test: check bin target

--- test/index.ts
+++ test/index.ts
@@ -3,21 +3,32 @@ import test = require('tape')
 import path = require('path')
 import exists = require('path-exists')
 import tempy = require('tempy')
+import fs = require('mz/fs')
 
 const fixtures = path.join(__dirname, 'fixtures')
 
 test('linkBins()', async (t) => {
   const binTarget = tempy.directory()
+  t.comment(`linking bins to ${binTarget}`)
+
   await linkBins(path.join(fixtures, 'node_modules'), binTarget)
 
-  t.ok(await exists(path.join(binTarget, 'simple')))
+  const binLocation = path.join(binTarget, 'simple')
+  t.ok(await exists(binLocation))
+  const content = await fs.readFile(binLocation, 'utf8')
+  t.ok(content.includes('node_modules/simple/index.js'))
   t.end()
 })
 
 test('linkPackageBins()', async (t) => {
   const binTarget = tempy.directory()
+  t.comment(`linking bins to ${binTarget}`)
+
   await linkPackageBins(path.join(fixtures, 'node_modules/simple'), binTarget)
 
-  t.ok(await exists(path.join(binTarget, 'simple')))
+  const binLocation = path.join(binTarget, 'simple')
+  t.ok(await exists(binLocation))
+  const content = await fs.readFile(binLocation, 'utf8')
+  t.ok(content.includes('node_modules/simple/index.js'))
   t.end()
 })
